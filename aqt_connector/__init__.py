2ba677e85c373317e87729c7b30721f110428235
Joe Rixon <joe.rixon@aqt.eu>
2025-03-14 11:34:51 +0100

    Rename application package

--- aqt_connector/__init__.py
+++ aqt_connector/__init__.py
@@ -1,6 +1,6 @@
+from aqt_connector._application.authentication import get_access_token as get_access_token
+from aqt_connector._application.authentication import log_in as log_in
 from aqt_connector._arnica_app import ArnicaApp as ArnicaApp
-from aqt_connector._primary_ports.authentication import get_access_token as get_access_token
-from aqt_connector._primary_ports.authentication import log_in as log_in
 from aqt_connector._sdk_config import ArnicaConfig as ArnicaConfig
 
 __all__ = [
--- aqt_connector/__main__.py
+++ aqt_connector/__main__.py
@@ -1,4 +1,4 @@
-from aqt_connector._primary_ports.cli import app
+from aqt_connector._application.cli import app
 
 if __name__ == "__main__":
     app()
