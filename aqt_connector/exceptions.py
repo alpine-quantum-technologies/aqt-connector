31d4e71775392e981362912734aa51c9dd0703b4
Joe Rixon <joe.rixon@aqt.eu>
2025-05-06 15:40:54 +0200

    Add PermissionError

--- aqt_connector/exceptions.py
+++ aqt_connector/exceptions.py
@@ -4,3 +4,7 @@ class TokenValidationError(Exception):
 
 class AuthenticationError(Exception):
     """A failure to authenticate a user."""
+
+
+class PermissionError(Exception):
+    """A failure to authorize a user."""
